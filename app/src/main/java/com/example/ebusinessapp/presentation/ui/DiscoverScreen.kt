4ec6e96dc28195f160cb057be152601c2ce96466
Hasan Cihad Altay <caltayzzz@gmail.com>
2023-11-30 19:42:53 +0300

    search place DONE

--- app/src/main/java/com/example/ebusinessapp/presentation/ui/DiscoverScreen.kt
+++ app/src/main/java/com/example/ebusinessapp/presentation/ui/DiscoverScreen.kt
@@ -13,6 +13,7 @@ import androidx.fragment.app.Fragment
 import com.example.ebusinessapp.R
 import com.example.ebusinessapp.databinding.FragmentDiscoverScreenBinding
 import com.example.ebusinessapp.util.Constants
+import com.google.android.gms.common.api.Status
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -22,6 +23,10 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.TileOverlayOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import com.google.maps.android.heatmaps.HeatmapTileProvider
 import kotlin.random.Random
 
@@ -29,6 +34,7 @@ class DiscoverScreen : Fragment(), OnMapReadyCallback {
 
     private lateinit var binding: FragmentDiscoverScreenBinding
     private var mGoogleMap: GoogleMap? = null
+    private lateinit var autocompleteFragment: AutocompleteSupportFragment
     private val PERMISSION_REQUEST_LOCATION = 1001
 
     override fun onCreateView(
@@ -50,9 +56,31 @@ class DiscoverScreen : Fragment(), OnMapReadyCallback {
     }
 
     private fun setMap() {
+        Places.initialize(requireContext().applicationContext, getString(R.string.google_maps_api_key))
+        autocompleteFragment = childFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.ADDRESS, Place.Field.LAT_LNG))
+        autocompleteFragment.setOnPlaceSelectedListener(object: PlaceSelectionListener {
+            override fun onError(p0: Status) {
+                Toast.makeText(requireContext(), "Some Error in Search", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPlaceSelected(place: Place) {
+                // val add = place.address
+                // val id = place.id
+                val latLng = place.latLng
+                zoomOnMap(latLng)
+            }
+
+        })
+
         val mapFragment =
             childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
+    }
+
+    private fun zoomOnMap(latLng: LatLng) {
+        val newLaLngZoom = CameraUpdateFactory.newLatLngZoom(latLng, 12f)
+        mGoogleMap?.animateCamera(newLaLngZoom)
     }
 
     private fun checkLocationPermission() {
